4d70efea223f8dcae39851dc48c7c1aee8db0bd6
Eliel Lopes <eliel.lds@gmail.com>
2021-10-19 18:20:15 -0300

    first commit

---
+++ src/main/java/br/com/rd/projetoVelhoLuxo/ProjetoVelhoLuxo.java
@@ -0,0 +1,13 @@
+package br.com.rd.projetoVelhoLuxo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoVelhoLuxo {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoVelhoLuxo.class, args);
+	}
+
+}
