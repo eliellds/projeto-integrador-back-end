1beb1cce6427f354c5a7d08a1d5b50cf14f0bc0e
RDJoseDeSouza <jose441232@gmail.com>
2021-10-24 18:03:38 -0300

    Crud usuarios completo

--- src/main/java/br/com/rd/projetoVelhoLuxo/controller/UserController.java
+++ src/main/java/br/com/rd/projetoVelhoLuxo/controller/UserController.java
@@ -1,4 +1,40 @@
 package br.com.rd.projetoVelhoLuxo.controller;
 
+import br.com.rd.projetoVelhoLuxo.model.dto.UserDTO;
+import br.com.rd.projetoVelhoLuxo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+    @Autowired
+    UserService service;
+    @PostMapping
+    private UserDTO create(@RequestBody UserDTO create){
+        return service.createUser(create);
+    }
+
+    @PutMapping
+    private UserDTO update(@RequestBody UserDTO update){
+        return service.update(update);
+    }
+    @GetMapping
+    private List<UserDTO> showList(){
+        return service.showList();
+    }
+
+    @GetMapping("/{id}")
+    private UserDTO findUser(@PathVariable("id") Long id){
+        return service.findById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    private  void deleteById(@PathVariable("id") Long id){
+         service.deleteById(id);
+    }
+
+
 }
--- src/main/java/br/com/rd/projetoVelhoLuxo/service/UserService.java
+++ src/main/java/br/com/rd/projetoVelhoLuxo/service/UserService.java
@@ -4,20 +4,127 @@ import br.com.rd.projetoVelhoLuxo.model.dto.TelephoneDTO;
 import br.com.rd.projetoVelhoLuxo.model.dto.UserDTO;
 import br.com.rd.projetoVelhoLuxo.model.entity.Telephone;
 import br.com.rd.projetoVelhoLuxo.model.entity.User;
+import br.com.rd.projetoVelhoLuxo.repository.contract.TelephoneRepository;
 import br.com.rd.projetoVelhoLuxo.repository.contract.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class UserService {
     @Autowired
     UserRepository userRepository;
+    @Autowired
+    TelephoneRepository telephoneRepository;
 
     public UserDTO createUser(UserDTO toCreate){
         User create= convertToUser(toCreate);
+        //verifica se tem o "produto"
+        if(create.getTelephone()!=null){
+            //verifica se tem um id
+            if (create.getTelephone().getId() != null) {
+                //verifica se esse id existe no banco
+                if(telephoneRepository.existsById(create.getTelephone().getId())){
+                    create.setTelephone(telephoneRepository.getById(create.getTelephone().getId()));
+
+                }
+
+
+
+            //se ele não ta nulo mas não tem o id ele tem os atributos
+            }else{
+                //salva os atributos como um novo "produto" e recebe um id
+                Telephone savedTel = telephoneRepository.save(create.getTelephone());
+                //adiciona de volta a tabela principal
+                create.setTelephone(savedTel);
+            }
+        }
         create = userRepository.save(create);
         return convertToDTO(create);
     }
+
+    //pegar lista de usuario
+
+
+    //update user
+    public UserDTO update (UserDTO toUpdate) {
+        if (toUpdate.getId() != null) {
+            if (userRepository.existsById(toUpdate.getId())) {
+                User update = userRepository.getById(toUpdate.getId());
+                //data de nascimento
+                if (toUpdate.getBorn() != null) {
+                    update.setBorn(toUpdate.getBorn());
+                }
+                //primeiro nome
+                if (toUpdate.getFirstName() != null) {
+                    update.setFirstName(toUpdate.getFirstName());
+                }
+                //sobrenome
+                if (toUpdate.getLastName() != null) {
+                    update.setLastName(toUpdate.getLastName());
+                }
+                //cpf
+//                if (toUpdate.getCpf() != null) { onde for unique key ele da erro se atualiza para o mesmo valor
+//                    update.setCpf(toUpdate.getCpf());
+//                }
+
+                if (toUpdate.getTelephone()!=null){
+                    Telephone telephoneToUpdate = convertToTelephone(toUpdate.getTelephone());
+                    if (telephoneToUpdate.getId() != null) {
+
+                        //verifica se existe esse id
+                        if(telephoneRepository.existsById(telephoneToUpdate.getId())) {
+                            //verfica se tem um numero para atualizar
+                            if(telephoneToUpdate.getNumber()!=null){
+                                telephoneToUpdate = telephoneRepository.save(telephoneToUpdate);
+                                //se não tiver, recupera o que tem do banco
+                            }else{
+                                telephoneToUpdate = telephoneRepository.getById(telephoneToUpdate.getId());
+                            }
+                        }
+                        //se ele não tem um id ele tem um numero
+                    }else{
+                        telephoneToUpdate = telephoneRepository.save(telephoneToUpdate);
+
+                    }
+                    //ao fim das verificações adiciona no update
+                    update.setTelephone(telephoneToUpdate);
+                }
+                update = userRepository.save(update);
+                return convertToDTO(update);
+            }
+
+
+        }
+        return null;
+    }
+
+    public List<UserDTO> showList(){
+        List<User> list = userRepository.findAll();
+
+        return convertListToDTO(list);
+    }
+
+    //recuperar usuario
+    public UserDTO findById(Long id){
+        if(userRepository.existsById(id)){
+
+            return convertToDTO( userRepository.getById(id));
+        }
+        return null;
+
+    }
+    //deletenaod usuario
+    public void deleteById(Long id){
+        if(userRepository.existsById(id)){
+
+             userRepository.deleteById(id);
+        }
+
+    }
+
 
     //conversões
     private UserDTO convertToDTO(User toConvert){
@@ -37,7 +144,10 @@ public class UserService {
         //id
         converted.setId(toConvert.getId());
         //telephone
-        converted.setTelephone(convertToDTOTelphone(toConvert.getTelephone()));
+        if(toConvert.getTelephone()!=null){
+            converted.setTelephone(convertToDTOTelphone(toConvert.getTelephone()));
+        }
+
 
         return converted;
 
@@ -60,25 +170,45 @@ public class UserService {
         //id
         converted.setId(toConvert.getId());
         //telephone
-        converted.setTelephone(convertToTelephone(toConvert.getTelephone()));
-
+        if(toConvert.getTelephone()!=null) {
+            converted.setTelephone(convertToTelephone(toConvert.getTelephone()));
+        }
         return converted;
 
+    }
+    //convert list
+    private List<UserDTO> convertListToDTO (List<User> toConvert){
+        List<UserDTO> converted = new ArrayList<>();
+
+        for (User a: toConvert) {
+            converted.add(convertToDTO(a));
+
+        }
+        return converted;
     }
 
     /////////////conversões de telefone////////
     //convert Para telefone final
     private Telephone convertToTelephone(TelephoneDTO toConvert){
         Telephone converted = new Telephone();
-        converted.setNumber(toConvert.getNumber());
+        if (toConvert.getId()!=null){
+            converted.setId(toConvert.getId());
+        }
+        if(toConvert.getNumber()!=null){
+            converted.setNumber(toConvert.getNumber());
+        }
         return converted;
     }
 
     //convert telefone DTO
     private TelephoneDTO convertToDTOTelphone(Telephone toConvert){
         TelephoneDTO converted = new TelephoneDTO();
-        converted.setId(toConvert.getId());
-        converted.setNumber(toConvert.getNumber());
+        if(toConvert.getId()!=null) {
+            converted.setId(toConvert.getId());
+        }
+        if(toConvert.getNumber()!=null) {
+            converted.setNumber(toConvert.getNumber());
+        }
         return converted;
     }
 }
