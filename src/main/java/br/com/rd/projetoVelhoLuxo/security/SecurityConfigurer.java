5772539b8d183b0b9d78d62f3e59f1e15d77a66b
Eliel Lopes <85817324+eliellds@users.noreply.github.com>
2021-10-24 01:36:05 -0300

    adicionando end-points de acesso sem login

--- src/main/java/br/com/rd/projetoVelhoLuxo/security/SecurityConfigurer.java
+++ src/main/java/br/com/rd/projetoVelhoLuxo/security/SecurityConfigurer.java
@@ -31,7 +31,7 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
-                .authorizeRequests().antMatchers("/authenticate").permitAll()
+                .authorizeRequests().antMatchers("/authenticate", "/contacts", "/products").permitAll()
                 .anyRequest().authenticated()
                 .and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
