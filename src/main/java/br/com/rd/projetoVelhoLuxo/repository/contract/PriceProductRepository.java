4e0081da14b60c9122765d64b576045017c024f9
Natalia Floridi <nataliafloridi@gmail.com>
2021-10-27 19:41:57 -0300

    adicionada QUERY para pesquisa de PREÇO PRODUTO MENOR PARA MAIOR

--- src/main/java/br/com/rd/projetoVelhoLuxo/repository/contract/PriceProductRepository.java
+++ src/main/java/br/com/rd/projetoVelhoLuxo/repository/contract/PriceProductRepository.java
@@ -5,6 +5,10 @@ import br.com.rd.projetoVelhoLuxo.model.entity.PriceProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PriceProductRepository extends JpaRepository <PriceProduct, PriceProductKey> {
+
+    List<PriceProduct> findByOrderByPriceAsc();
 }
