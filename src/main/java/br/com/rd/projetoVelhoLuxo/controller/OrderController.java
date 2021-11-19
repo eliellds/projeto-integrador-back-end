abf3faec3d4aed0e88fe83ac06f2c61ca12cd615
Natalia Floridi <nataliafloridi@gmail.com>
2021-11-18 22:52:37 -0300

    solucuinado erro de cors na order controller

--- src/main/java/br/com/rd/projetoVelhoLuxo/controller/OrderController.java
+++ src/main/java/br/com/rd/projetoVelhoLuxo/controller/OrderController.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/orders")
+@CrossOrigin(origins = "http://localhost:3000", allowedHeaders = "*")
 public class OrderController {
     @Autowired
     OrderService service;
